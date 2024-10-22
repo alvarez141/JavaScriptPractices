d44ab6f12b6ead8d8748ce603cd9e4c74da9ee9f
alvarez141 <alvarezdanieldelgadillo@gmail.com>
2024-10-22 02:56:44 +0200

    hosting, conatntes

---
+++ constantes.js
@@ -0,0 +1,10 @@
+//Constantes
+const Mi_CONSTANTE = "Juan"; // una constante siempre se debe declarar
+//Mi_CONSTANTE = "Pedro"; // no podemos cambiar el valor de una constante
+console.log(Mi_CONSTANTE);
+
+console.log(Math.PI);
+
+// contante de segundos por minutos
+const SEGUNDOS_POR_MINUTO = 60;
+console.log(SEGUNDOS_POR_MINUTO);
---
+++ hosting.js
@@ -0,0 +1,10 @@
+'use strict' // define que se debe usar el modo estricto de Hosting
+//Hosting(Podemos usar una variable y luego utilizarla solo es utlizado con var)
+//var x; //Declaración de la variable
+x = 5; //Inicialización de la variable
+//y = 10; // al usar let no aplica el concepto de hosting
+console.log(x);
+
+
+var x;
+let y;
